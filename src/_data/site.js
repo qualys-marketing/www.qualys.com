8d014bd9e369ae435562906a71a20c729e0005d7
Abdullah Yahya <ayahya@qualys.com>
2023-05-18 15:41:46 -0700

    fixed path to local .env file

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const dotenv = require('dotenv');
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
 
 /*
  * site wide data
