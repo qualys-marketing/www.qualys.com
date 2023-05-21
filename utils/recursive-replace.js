45f3550eed94c0c41033945966389b3c144c63d0
Abdullah Yahya <ayahya@qualys.com>
2023-05-21 16:52:56 -0700

    updated migration script

--- utils/recursive-replace.js
+++ utils/recursive-replace.js
@@ -32,72 +32,72 @@ const edit = filePath => {
   var oldContent = fs.readFileSync(filePath, {encoding: 'utf8'});
 
   // REPLACE {{#if class}} with {% if class %}
-  var regex = /\{\{#if\s+([a-zA-Z0-9-_]+)?\}\}/;
+  var regex = /\{\{#if\s+([a-zA-Z0-9-_]+)?\}\}/gi;
   var replaceVal = '{% if $1 %}';
   var newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{/if}} with {% endif %}
   oldContent = newContent;
-  regex = '{{/if}}';
+  regex = '/\{\{\/if\}\}/gi';
   replaceVal = '{% endif %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{@root.site.asset}} with {{site.asset}}
   oldContent = newContent;
-  regex = '{{@root.site.asset}}';
+  regex = '/\{\{@root\.site\.asset\}\}/gi';
   replaceVal = '{{site.asset}}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{{class}}} with {{ class | safe }}
   oldContent = newContent;
-  regex = '/\{\{\{\s*([a-zA-Z0-9-_]+)?\s*\}\}\}/';
+  regex = '/\{\{\{\s*([a-zA-Z0-9-_]+)?\s*\}\}\}/gi';
   replaceVal = '{{ $1 | safe }}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE @first with loop.first
   oldContent = newContent;
-  regex = '@first';
+  regex = '/@first/gi';
   replaceVal = 'loop.first';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{!-- with {#
   oldContent = newContent;
-  regex = '{{!--';
+  regex = '/\{\{!--/gi';
   replaceVal = '{#';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE --}} with #}
   oldContent = newContent;
-  regex = '--}}';
+  regex = '/--\}\}/gi';
   replaceVal = '#}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> (lookup . 'ctaPartial')}} with {% include ctaPartial %}
   oldContent = newContent;
-  regex = '/\{\{>\s*\(\s*lookup\s*\.\s*'([a-zA-Z0-9-_]+)?'\s*\)\s*\}\}/';
+  regex = '/\{\{>\s*\(\s*lookup\s*\.\s*\'([a-zA-Z0-9-_]+)?\'\s*\)\s*\}\}/gi';
   replaceVal = '{% include $1 %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> vendor/vimeo-player }} with {% include vendor/vimeo-player.njk %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*\}\}/';
+  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*\}\}/gi';
   replaceVal = '{% include $1.njk %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> apps/hero-list this}} with {% include apps/hero-list.njk %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*this\s*\}\}/';
+  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*this\s*\}\}/gi';
   replaceVal = '{% include $1.njk %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> social-list dark=true centered=true}} with {% include social-list.njk dark=true centered=true %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*([a-zA-Z0-9-_/=:"'\s]+)\s*\}\}/';
+  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*([a-zA-Z0-9-_/=:"\'\s]+)\s*\}\}/gi';
   replaceVal = '{% include $1.njk $2 %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // replace file extension from hbs to njk
-  filePath = filePath.replace(".hbs", ".njk");
+  // filePath = filePath.replace(".hbs", ".njk");
 
   fs.writeFileSync(filePath, newContent, {encoding: 'utf-8'});
   console.log(`Edited file: ${filePath}`);
