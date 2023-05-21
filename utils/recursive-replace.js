cc9253e903ecfc18926aed095405dd011df06bca
Abdullah Yahya <ayahya@qualys.com>
2023-05-21 16:58:21 -0700

    Fixed bug in migration script

--- utils/recursive-replace.js
+++ utils/recursive-replace.js
@@ -38,61 +38,61 @@ const edit = filePath => {
 
   // REPLACE {{/if}} with {% endif %}
   oldContent = newContent;
-  regex = '/\{\{\/if\}\}/gi';
+  regex = /\{\{\/if\}\}/gi;
   replaceVal = '{% endif %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{@root.site.asset}} with {{site.asset}}
   oldContent = newContent;
-  regex = '/\{\{@root\.site\.asset\}\}/gi';
+  regex = /\{\{@root\.site\.asset\}\}/gi;
   replaceVal = '{{site.asset}}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{{class}}} with {{ class | safe }}
   oldContent = newContent;
-  regex = '/\{\{\{\s*([a-zA-Z0-9-_]+)?\s*\}\}\}/gi';
+  regex = /\{\{\{\s*([a-zA-Z0-9-_]+)?\s*\}\}\}/gi;
   replaceVal = '{{ $1 | safe }}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE @first with loop.first
   oldContent = newContent;
-  regex = '/@first/gi';
+  regex = /@first/gi;
   replaceVal = 'loop.first';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{!-- with {#
   oldContent = newContent;
-  regex = '/\{\{!--/gi';
+  regex = /\{\{!--/gi;
   replaceVal = '{#';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE --}} with #}
   oldContent = newContent;
-  regex = '/--\}\}/gi';
+  regex = /--\}\}/gi;
   replaceVal = '#}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> (lookup . 'ctaPartial')}} with {% include ctaPartial %}
   oldContent = newContent;
-  regex = '/\{\{>\s*\(\s*lookup\s*\.\s*\'([a-zA-Z0-9-_]+)?\'\s*\)\s*\}\}/gi';
+  regex = /\{\{>\s*\(\s*lookup\s*\.\s*\'([a-zA-Z0-9-_]+)?\'\s*\)\s*\}\}/gi;
   replaceVal = '{% include $1 %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> vendor/vimeo-player }} with {% include vendor/vimeo-player.njk %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*\}\}/gi';
+  regex = /\{\{>\s*([a-zA-Z0-9-_/]+)?\s*\}\}/gi;
   replaceVal = '{% include $1.njk %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> apps/hero-list this}} with {% include apps/hero-list.njk %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*this\s*\}\}/gi';
+  regex = /\{\{>\s*([a-zA-Z0-9-_/]+)?\s*this\s*\}\}/gi;
   replaceVal = '{% include $1.njk %}';
   newContent = oldContent.replace(regex, replaceVal);
 
   // REPLACE {{> social-list dark=true centered=true}} with {% include social-list.njk dark=true centered=true %}
   oldContent = newContent;
-  regex = '/\{\{>\s*([a-zA-Z0-9-_/]+)?\s*([a-zA-Z0-9-_/=:"\'\s]+)\s*\}\}/gi';
+  regex = /\{\{>\s*([a-zA-Z0-9-_/]+)?\s*([a-zA-Z0-9-_/=:"\'\s]+)\s*\}\}/gi;
   replaceVal = '{% include $1.njk $2 %}';
   newContent = oldContent.replace(regex, replaceVal);
 
