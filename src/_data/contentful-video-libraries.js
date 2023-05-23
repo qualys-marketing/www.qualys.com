41f67f2565ca24b31bcbd566a4a962d90f3ec5d5
Abdullah Yahya <ayahya@qualys.com>
2023-05-22 20:07:00 -0700

    Add all Contentful JSON data entries in www space

---
+++ src/_data/contentful-video-libraries.js
@@ -0,0 +1,23 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+
+// Contentful API docs
+// https://www.contentful.com/developers/docs/references/content-delivery-api/
+const space_id = "4l0w8syj29ap";
+const environment_id = "master";
+const access_token = process.env.CONTENTFUL_ACCESS_TOKEN;
+const content_type = "videoLibraries";
+const limit = "1000"; // max API query results
+const url = `https://cdn.contentful.com//spaces/${space_id}/environments/${environment_id}/entries?access_token=${access_token}&limit=${limit}&content_type=${content_type}`;
+
+console.log(url);
+
+module.exports = async function() {
+    /* This returns a promise */
+    return EleventyFetch(url, {
+	    duration: "1d", // save for 1 day
+	    type: "json"    // we’ll parse JSON for you
+    });
+}; 
