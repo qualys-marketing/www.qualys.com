e67dd704941789b85c9c1f1a27a356e2a0bac35b
Abdullah Yahya <ayahya@qualys.com>
2023-05-25 17:38:47 -0700

    Commented out code, fixed redirect rules

--- src/company/privacy/index.11tydata.js
+++ src/company/privacy/index.11tydata.js
@@ -13,7 +13,7 @@ const select = "fields"
 const limit = "1000"; // max API query results
 var url = `https://cdn.contentful.com//spaces/${space_id}/environments/${environment_id}/entries?access_token=${access_token}&limit=${limit}&content_type=${content_type}&order=${order}&select=${select}&sys.id=${entry_id}`;
 
-console.log(url);
+// console.log(url);
 
 module.exports = async function() {
     /* This returns a promise */
@@ -29,7 +29,7 @@ module.exports = async function() {
 
 	var simpleData = {items: results};
 	
-	console.log(simpleData);
+	// console.log(simpleData);
 
 	return simpleData.items[0];
 
